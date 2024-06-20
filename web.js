b79a498a916cf1aca41d01f9c874eb970bfa97e1
sunlyhuor <jkhmer1111@gmail.com>
2024-06-20 23:32:42 +0700

    fix: added lang

--- web.js
+++ web.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const app = express();
-const path = require("path");
 const user = require('./routers/user');
 const userApi = require('./routers/api/user');
 const bodyParser = require("body-parser")
+const cookieParser = require('cookie-parser');
+const path = require("path")
+const fs = require("fs")
+const env = require("dotenv")
+
+// env
+env.config()
+
+function parseCookieString(cookieString) {
+    const cookieObj = {};
+    
+    // Split the string by '; ' to get individual key=value pairs
+    const pairs = cookieString.split('; ');
+
+    pairs.forEach(pair => {
+        // Split each pair by '=' to get the key and value
+        const [key, value] = pair.split('=');
+        // Assign the key-value pair to the object
+        cookieObj[key] = value;
+    });
+
+    return cookieObj;
+}
+
+// 
+app.use((req, res, next) => {
+    // res.json(parseCookieString(req.headers.cookie))
+    if( !parseCookieString(req.headers.cookie).LANG ){
+        res.cookie("LANG", process.env.DEFAULT_LANG, { maxAge: 24 * 60 * 60 * 2, httpOnly: true })
+    }
+    next()
+})
 
 // Middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
+
+// Use cookie-parser middleware
+app.use(cookieParser());
 
 // Public assets
 app.use(express.static(path.join(__dirname, 'views')));
@@ -21,6 +55,7 @@ app.use( user )
 app.use("/api", userApi)
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:' + port);
 });
