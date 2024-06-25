72e69b596f40fc7334cfd2cd352eedd2c113d009
sunlyhuor <jkhmer1111@gmail.com>
2024-06-26 02:58:17 +0700

    fix: added authorize in middlewae

--- routers/api/user.js
+++ routers/api/user.js
@@ -1,10 +1,11 @@
 const express = require("express")
+const { Sign } = require("../../libs/jwt")
 const routers = express.Router()
 
 
 routers.post("/login", (req, res) => {
-    res.cookie("accessToken", "sdhasjdhj")
-    res.cookie("refreshToken", "sdhasjdhj")
+    res.cookie("accessToken", Sign( { name: req.body.name, role:"admin" }, process.env.JWT_ACCESS_SECRET, process.env.JWT_ACCESS_DURATION ) )
+    res.cookie("refreshToken",  Sign( { name: req.body.name, role: "admin" }, process.env.JWT_REFRESH_SECRET, process.env.JWT_REFRESH_DURATION ) )
     res.cookie("isLogin", true)
     res.redirect("/")
 })  
